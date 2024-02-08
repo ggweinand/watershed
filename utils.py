3178d848e963b983d2bec0ff13b10ea88956d995
ggweinand <ggweinand@gmail.com>
2024-02-08 12:11:56 +0100

    Watershed implementations with markers. * Added priority queue to utils. * Created requirements.txt file

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+
+import heapq
+
 from enum import Enum
 from typing import List
 
@@ -15,20 +18,36 @@ class Coordinate:
     def __repr__(self) -> str:
         return f"Coordinate({self.h}, {self.w})"
 
-    def __eq__(self, another):
+    def __eq__(self, another: Coordinate):
         return (self.h, self.w) == (another.h, another.w)
 
     def __hash__(self):
         return hash((self.h, self.w))
 
-    def valid(self, limits) -> bool:
+    def valid(self, limits: tuple[int, int]) -> bool:
         limit_h, limit_w = limits
         valid_h = 0 <= self.h and self.h < limit_h
         valid_w = 0 <= self.w and self.w < limit_w
         return valid_h and valid_w
 
-    def neighbors(self, limits) -> List[Coordinate]:
+    def neighbors(self, limits: tuple[int, int]) -> List[Coordinate]:
         move_h = [-1, 0, 1, -1, 1, -1, 0, 1]
         move_w = [-1, -1, -1, 0, 0, 1, 1, 1]
         unfiltered = [Coordinate(self.h + dh, self.w + dw) for dh, dw in zip(move_h, move_w)]
         return [c for c in unfiltered if c.valid(limits)]
+
+class PriorityQueue:
+    def __init__(self):
+        self.h: List[tuple[int, int, Coordinate]] = []
+        self.entry: int = 0
+
+    def push(self, c: Coordinate, value: int):
+        heapq.heappush(self.h, (value, self.entry, c))
+        self.entry += 1
+
+    def pop(self) -> Coordinate:
+        _, _, c = heapq.heappop(self.h)
+        return c
+
+    def empty(self) -> bool:
+        return not self.h
--- watershed.py
+++ watershed.py
@@ -2,8 +2,9 @@ import numpy as np
 import numpy.typing as npt
 
 from collections import deque
+from typing import List
 
-from utils import Coordinate, State
+from utils import Coordinate, State, PriorityQueue
 
 # Type alias for int numpy arrays.
 NDArrayInt = npt.NDArray[np.int_]
@@ -82,3 +83,131 @@ def watershed(image:NDArrayInt) -> NDArrayInt:
                 result[h][w] = 255
 
     return result
+
+
+def watershed_markers(image:NDArrayInt, markers:NDArrayInt) -> tuple[NDArrayInt, NDArrayInt]:
+    height, width = limits = image.shape
+
+    state = {}
+    label = {}
+    queue = deque()
+    flag = False
+
+    max_level = np.max(image)
+    level_coord = [[] for _ in range(max_level + 1)]
+
+    for h in range(height):
+        for w in range(width):
+            c = Coordinate(h,w)
+            level_coord[image[h, w]].append(c)
+            if markers[h,w]:
+                state[c] = State.BASIN
+                label[c] = markers[h,w]
+            else:
+                state[c] = State.INIT
+                label[c] = 0
+
+    for current_level in level_coord:
+        for c in current_level:
+            # If a neighbor is a basin or a watershed, add the current pixel to the queue.
+            l = [n for n in c.neighbors(limits) if state[n] in [State.WSHED, State.BASIN]]
+            if l:
+                queue.append(c)
+                state[c] = State.INQUEUE
+
+        # Extend existing basins.
+        while queue:
+            c = queue.popleft()
+            for n in c.neighbors(limits):
+                match state[n]:
+                    case State.BASIN:
+                        if state[c] is State.INQUEUE or (state[c] is State.WSHED and flag):
+                            label[c] = label[n]
+                            state[c] = State.BASIN
+                        if state[c] is State.BASIN and label[c] != label[n]:
+                            state[c] = State.WSHED
+                            flag = False
+                    case State.WSHED:
+                        if state[c] is State.INQUEUE:
+                            state[c] = State.WSHED
+                            flag = True
+                    case State.MASK:
+                        state[n] = State.INQUEUE
+                        queue.append(n)
+
+    # Construct segment image.
+    region = np.empty_like(image)
+    wshed = np.empty_like(image)
+    for h in range(height):
+        for w in range(width):
+            if state[Coordinate(h,w)] is State.WSHED:
+                wshed[h][w] = 0
+            else:
+                wshed[h][w] = 255
+            region[h][w] = label[Coordinate(h,w)]
+
+    return region, wshed
+
+
+def watershed_markers_cv(image:NDArrayInt, marker:NDArrayInt) -> tuple[NDArrayInt, NDArrayInt]:
+    height, width = limits = image.shape
+
+    state: dict[Coordinate, State] = {}
+    label: dict[Coordinate, int] = {}
+    p_queue = PriorityQueue()
+    markers: List[Coordinate] = []
+    flag = False
+
+    for h in range(height):
+        for w in range(width):
+            c = Coordinate(h,w)
+            if marker[h,w]:
+                state[c] = State.BASIN
+                label[c] = marker[h,w]
+                markers.append(c)
+            else:
+                state[c] = State.INIT
+
+    for c in markers:
+        for n in c.neighbors(limits):
+            if state[n] is State.INIT:
+                p_queue.push(n, image[n.h, n.w])
+                state[n] = State.INQUEUE
+
+    while not p_queue.empty():
+        c = p_queue.pop()
+
+        for n in c.neighbors(limits):
+            match state[n]:
+                case State.BASIN:
+                    if state[c] is State.INQUEUE or (state[c] is State.WSHED and flag):
+                        label[c] = label[n]
+                        state[c] = State.BASIN
+                    if state[c] is State.BASIN and label[c] != label[n]:
+                        label[c] = 0
+                        state[c] = State.WSHED
+                        flag = False
+                case State.WSHED:
+                    if state[c] is State.INQUEUE:
+                        label[c] = 0
+                        state[c] = State.WSHED
+                        flag = True
+
+        if state[c] is State.BASIN:
+            for n in c.neighbors(limits):
+                if state[n] is State.INIT:
+                    state[n] = State.INQUEUE
+                    p_queue.push(n, image[n.h, n.w])
+
+    # Construct segment image.
+    region = np.empty_like(image)
+    wshed = np.empty_like(image)
+    for h in range(height):
+        for w in range(width):
+            if state[Coordinate(h,w)] is State.WSHED:
+                wshed[h,w] = 0
+            else:
+                wshed[h,w] = 255
+            region[h,w] = label[Coordinate(h,w)]
+
+    return region, wshed
